0189a9b8cec2ca28eec734cdc20f0790b0df0aa5
Kahvi Patel <iamkahvi@gmail.com>
2020-01-24 09:42:30 -0600

    KP: Rendering table function

--- frontend/bow/src/pages/Home.js
+++ frontend/bow/src/pages/Home.js
@@ -1,19 +1,84 @@
 import React, { Component } from "react";
 import Layout from "../components/Layout";
 
+const savingsData = [
+  [
+    {
+      date: "2020-01-01T06:00:00.000Z",
+      type: "Deposit",
+      amount: 100,
+      title: "Misc"
+    },
+    {
+      date: "2020-01-01T06:00:00.000Z",
+      type: "Deposit",
+      amount: 100,
+      title: "Misc"
+    },
+    {
+      date: "2020-01-01T06:00:00.000Z",
+      type: "Deposit",
+      amount: 100,
+      title: "Misc"
+    },
+    {
+      date: "2020-01-01T06:00:00.000Z",
+      type: "Deposit",
+      amount: 100,
+      title: "Misc"
+    }
+  ]
+];
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      files: [],
-      download: ""
+      savings: savingsData,
+      checking: []
     };
   }
 
+  componentDidMount = async () => {
+    // if (!localStorage.getItem("guugle-login-token")) {
+    //   this.props.history.push("/login");
+    //   return;
+    // }
+    // const link = "http://localhost:3001/list";
+    // fetch(link, {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: localStorage.getItem("guugle-login-token")
+    //   }
+    // }).then(res => {
+    //   if (res.status === 200) {
+    //     res.json().then(json => this.setState({ files: json }));
+    //   } else {
+    //     res.text().then(text => alert(text));
+    //   }
+    // });
+    this.setState({
+      savings: savingsData
+    });
+  };
+
+  renderTable(account) {
+    if (this.state[account].length === 0) {
+      return <div>You have no transactions</div>;
+    } else {
+      const items = this.state[account].map(el => {
+        return <p>hello</p>;
+      });
+    }
+    return <p>error</p>;
+  }
+
   render() {
+    console.log("savings", this.state.savings);
     return (
       <Layout>
         <h3>Transactions</h3>
+        {this.renderTable("savings")}
       </Layout>
     );
   }
