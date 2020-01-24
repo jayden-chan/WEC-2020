4533194e747a1bc0e2af512863727f2861e0dc58
Kahvi Patel <iamkahvi@gmail.com>
2020-01-24 10:47:35 -0600

    KP: adding transactions

--- frontend/bow/src/pages/Home.js
+++ frontend/bow/src/pages/Home.js
@@ -6,6 +6,13 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 
 const savingsData = [
   {
@@ -61,13 +68,26 @@ const chequingData = [
   }
 ];
 
+const StyledFormControl = withStyles({
+  root: {
+    width: "200px",
+    margin: "0 10px"
+  }
+})(FormControl);
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       savings: [],
-      chequing: []
+      chequing: [],
+      transactionDate: "",
+      transactionType: "",
+      transactionAmount: 0,
+      transactionAccount: "",
+      transactionTitle: ""
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount = async () => {
@@ -94,6 +114,16 @@ export default class Home extends Component {
     });
   };
 
+  handleSubmit() {
+    const body = JSON.stringify({
+      i_type: this.state.transactionType,
+      acc: this.state.transactionAccount,
+      amount: this.state.transactionAmount,
+      title: this.state.transactionTitle
+    });
+    console.log("body", body);
+  }
+
   renderTable(account) {
     if (this.state[account].length === 0) {
       return <div>You have no transactions</div>;
@@ -114,7 +144,7 @@ export default class Home extends Component {
 
       return (
         <TableContainer>
-          <Table>
+          <Table stickyHeader aria-label="sticky table">
             <TableHead>
               <TableRow>
                 <TableCell>Type</TableCell>
@@ -131,8 +161,65 @@ export default class Home extends Component {
     return <p>error</p>;
   }
 
+  transactionForm() {
+    return (
+      <form noValidate autoComplete="off">
+        <TextField
+          id="transaction-title"
+          label="Title"
+          type="text"
+          value={this.state.transactionTitle}
+          onChange={e => {
+            this.setState({ transactionTitle: e.target.value });
+          }}
+        />
+
+        <StyledFormControl size="medium">
+          <InputLabel>Type</InputLabel>
+          <Select
+            id="transaction-type"
+            size="medium"
+            value={this.state.transactionType}
+            onChange={e => {
+              this.setState({ transactionType: e.target.value });
+            }}
+          >
+            <MenuItem value="Withdrawal">Withdrawal</MenuItem>
+            <MenuItem value="Deposit">Deposit</MenuItem>
+          </Select>
+        </StyledFormControl>
+        <StyledFormControl size="medium">
+          <InputLabel>Account</InputLabel>
+          <Select
+            size="medium"
+            id="transaction-account"
+            value={this.state.transactionAccount}
+            onChange={e => {
+              this.setState({ transactionAccount: e.target.value });
+            }}
+          >
+            <MenuItem value="s">Savings</MenuItem>
+            <MenuItem value="c">Chequing</MenuItem>
+          </Select>
+        </StyledFormControl>
+        <TextField
+          id="transaction-amount"
+          label="Amount"
+          type="number"
+          value={this.state.transactionAmount}
+          onChange={e => {
+            this.setState({ transactionAmount: e.target.value });
+          }}
+          inputProps={{ min: "0" }}
+        />
+        <Button onClick={this.handleSubmit} variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
+    );
+  }
+
   render() {
-    console.log("savings", this.state.savings);
     return (
       <Layout>
         <h3>Transactions</h3>
@@ -140,6 +227,8 @@ export default class Home extends Component {
         {this.renderTable("savings")}
         <h4>Chequing</h4>
         {this.renderTable("chequing")}
+        <h4>Add Transaction</h4>
+        {this.transactionForm()}
       </Layout>
     );
   }
